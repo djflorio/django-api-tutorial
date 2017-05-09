1298de278fd738d95fdebe54426e6e17dff0ed3e
Dan Florio <dflo@danflorio.com>
2017-05-09 15:49:01 -0400

    refactor to take advantage of framework

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+# This allows us to specify a format (such as .json) at the
+# end of the URL. For example, localhost:8000/snippets/1.json
+urlpatterns = format_suffix_patterns(urlpatterns)
