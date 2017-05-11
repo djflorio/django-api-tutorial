a3465dd6652923b77835c12e51e7694a6d64f2eb
Dan Florio <dflo@danflorio.com>
2017-05-11 11:02:55 -0400

    add hyperlink navigation between objects

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,6 +17,4 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
-    url(r'^api-auth/', include('rest_framework.urls',
-                              namespace='rest_framework')),
 ]
