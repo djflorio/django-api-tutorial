242699f5f36397fa07235970d04d74c37009b27e
Dan Florio <dflo@danflorio.com>
2017-05-12 12:03:33 -0400

    add url router and use viewsets

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,21 +1,17 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework.routers import DefaultRouter
 
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    url(r'^$', views.api_root),
-    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-]
-
-# This allows us to specify a format (such as .json) at the
-# end of the URL. For example, localhost:8000/snippets/1.json
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
 ]
